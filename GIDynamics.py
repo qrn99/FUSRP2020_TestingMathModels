c218a18d51e0eb71458b95e6cde9415b2641b54b
Lily Y <lilyxlyang@outlook.com>
2020-07-31 20:12:15 -0400

    small I_b, intuitively corrent glucose curve

--- GIDynamics.py
+++ GIDynamics.py
@@ -4,56 +4,66 @@ Class of functions simulating Glucose and Insuline dynamics
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+from typing import List
 
-k_js = 10**(-0.8)
-k_gj = 10**(-1)
-k_jl = 10**(-1.5)
-tao = 10**2
-k_gl = 10**(-0.8)
-k_xg = 10**(-1.9)
-k_xgi = 10**(-7)
-eta = 10**(-1.9)
-k_lambda = 10**(-1.3)
-f_gj = 10**0.8
-k_xi = 10**(-1.9)
-gamma = 10**0.2
-beta = 10**2
-D = 10**2.5
-G_b = 10
-I_b = 10**1.8
-G_prod0 = -G_b
-
+k_js = 0.1
+k_gj = 0.1
+k_jl = 0.0316
 tau = 50
+k_gl = 0.1
+k_xg = 0.01
+k_xgi = 10**(-6.5)
+eta = 10**(-1.9)
+k_lambda = 0.0316
+f_gj = 10
+k_xi = 0.01
+gamma = 5
+beta = 100
+D = 10**2.5
+G_b = 7
+I_b = 1.7*10**(-7)
+G_prod0 = k_xgi*G_b*I_b+k_xg*G_b
+J = [0]*120
+
+
+def time_delay(t, J):
+    if t < tau:
+        return 0
+    else:
+        return J[round(t-tau)]
+
+
+def Gprod(g):
+    return k_lambda / (k_lambda / G_prod0 + (g - G_b))
+
 
 def gi(x, t):
     """
     gi dynamics complete
-    :param x:
-    :type x:
-    :param t:
-    :type t:
-    :return:
-    :rtype:
+    args are the input params
     """
+    # k_js, k_gj, k_jl, tau, k_gl, k_xg, k_xgi, eta, k_lambda, f_gj, k_xi, gamma, beta, D, G_b, I_b = args
+
     s = x[0]
     j = x[1]
     l = x[2]
     g = x[3]
     i = x[4]
-    gprod = k_lambda/(k_lambda/G_prod0+(g-G_b))
-
-    phi = 0 if t < tao else j * (t - tau)
 
     dsdt = -k_js * s
     djdt = k_js * s - k_gj * j - k_jl * j
-    dldt = k_jl * phi - k_gl * l
-    dgdt = -(k_xg + k_xgi*i)*g + gprod + eta*(k_gj*j + k_gl*l)
-    didt = k_xi*I_b*((beta**gamma+1)/(beta**gamma*(G_b/(g+f_gj*(k_gj*j + k_gl*l)))**gamma + 1)
-                     - i/I_b)
+    J[round(t)] = j
+    dldt = k_jl * time_delay(t, J) - k_gl * l
+    dgdt = -(k_xg + k_xgi * i)*g + Gprod(g) + eta*(k_gj*j + k_gl*l)
+    num = beta**gamma + 1
+    G_tilda = g + f_gj * (k_gj * j + k_gl * l)
+    denom = (beta**gamma) * (G_b/G_tilda)**gamma + 1
+    didt = k_xi * I_b * (num/denom - i/I_b)
     return [dsdt, djdt, dldt, dgdt, didt]
 
-x0 = [70, 0, 0, G_b, I_b]
-t = np.linspace(0, 2000, 5000)
+
+x0 = [417, 0, 0, G_b, I_b]
+t = np.linspace(0, 119, 119)
 x = odeint(gi, x0, t)
 
 s = x[:, 0]
@@ -62,10 +72,10 @@ l = x[:, 2]
 g = x[:, 3]
 i = x[:, 4]
 
-plt.plot(t, s, label='S')
-plt.plot(t, j, label='J')
-plt.plot(t, l, label='L')
+# plt.plot(t, s, label='S')
+# plt.plot(t, j, label='J')
+# plt.plot(t, l, label='L')
 plt.plot(t, g, label='G')
-plt.plot(t, i, label='I')
+# plt.plot(t, i, label='I')
 plt.legend()
 plt.show()
